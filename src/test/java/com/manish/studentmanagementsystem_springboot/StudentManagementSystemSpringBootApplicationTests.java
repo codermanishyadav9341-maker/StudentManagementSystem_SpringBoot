815352ffe7d208a85acf5d928a725967cc120d04
Manish_Yadav_08 <codermanishyadav9341@gmail.com>
2026-07-02 16:23:30 +0530

    Student Management System

---
+++ src/test/java/com/manish/studentmanagementsystem_springboot/StudentManagementSystemSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manish.studentmanagementsystem_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementSystemSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
