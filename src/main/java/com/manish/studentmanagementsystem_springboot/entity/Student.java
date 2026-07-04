fe554fc76a19b905277610a47d18f93f11395b25
Manish_Yadav_08 <codermanishyadav9341@gmail.com>
2026-07-04 16:28:47 +0530

    Initial commit

---
+++ src/main/java/com/manish/studentmanagementsystem_springboot/entity/Student.java
@@ -0,0 +1,19 @@
+package com.manish.studentmanagementsystem_springboot.entity;
+
+import java.sql.Date;
+public class Student {
+    private int id;
+    private String name;
+    private int age;
+    private String gender;
+    private String email;
+    private String phone;
+    private String address;
+    private Date dateOfBirth;
+    private Date admissionDate;
+    private String course;
+    private Double fee;
+    private String status;
+    private String createdAt;
+    private String updatedAt;
+}
