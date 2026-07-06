d9bf21c757b3f0a977f62940ec292e797e00a0cf
Manish_Yadav_08 <codermanishyadav9341@gmail.com>
2026-07-06 17:59:03 +0530

    Initial commit

--- src/main/java/com/manish/studentmanagementsystem_springboot/entity/Student.java
+++ src/main/java/com/manish/studentmanagementsystem_springboot/entity/Student.java
@@ -13,20 +13,27 @@ public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    @Column(nullable = false)
+    @Column(nullable = false,length = 15)
     private String name;
+    @Column(nullable = false)
     private int age;
+    @Column(nullable = false)
     private String gender;
     @Column(unique = true,nullable = false)
     private String email;
-    @Column(length = 10)
+    @Column(nullable = false,length = 10)
     private String phone;
+    @Column(nullable = false)
     private String address;
+    @Column(nullable = false)
     private LocalDate dateOfBirth;
+    @Column(nullable = false)
     private LocalDate admissionDate;
+    @Column(nullable = false)
     private String course;
     @Positive
     private Double fee;
+    @Column(nullable = false)
     private String status;
     private LocalDate createdAt;
     private LocalDate updatedAt;
