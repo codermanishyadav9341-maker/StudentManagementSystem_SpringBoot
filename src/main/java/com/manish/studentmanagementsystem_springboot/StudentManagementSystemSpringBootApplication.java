4aa4fcc840c72a37f3fdc4c2f33eed4ea4032839
Manish_Yadav_08 <codermanishyadav9341@gmail.com>
2026-07-08 17:18:38 +0530

    Initial commit

--- src/main/java/com/manish/studentmanagementsystem_springboot/StudentManagementSystemSpringBootApplication.java
+++ src/main/java/com/manish/studentmanagementsystem_springboot/StudentManagementSystemSpringBootApplication.java
@@ -9,5 +9,4 @@ public class StudentManagementSystemSpringBootApplication {
     public static void main(String[] args) {
         SpringApplication.run(StudentManagementSystemSpringBootApplication.class, args);
     }
-
 }
