89202cfb3a33fbfd8dbea6e75ccc9c49f4527010
Manish_Yadav_08 <codermanishyadav9341@gmail.com>
2026-07-08 16:55:57 +0530

    Initial commit

--- src/main/java/com/manish/studentmanagementsystem_springboot/dto/response/StudentResponse.java
+++ src/main/java/com/manish/studentmanagementsystem_springboot/dto/response/StudentResponse.java
@@ -1,9 +1,16 @@
 package com.manish.studentmanagementsystem_springboot.dto.response;
 
+import java.time.LocalDate;
 public class StudentResponse {
 
     private Long id;
     private String name;
     private Integer age;
     private String gender;
+    private String address;
+    private String email;
+    private String phone;
+    private LocalDate dateOfBirth;
+    private LocalDate admissionDate;
+
 }
