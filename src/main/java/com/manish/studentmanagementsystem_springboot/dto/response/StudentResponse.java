d7759106b1315116efc16f5e97744073532f9dc3
Manish_Yadav_08 <codermanishyadav9341@gmail.com>
2026-07-08 16:50:43 +0530

    Update code

---
+++ src/main/java/com/manish/studentmanagementsystem_springboot/dto/response/StudentResponse.java
@@ -0,0 +1,9 @@
+package com.manish.studentmanagementsystem_springboot.dto.response;
+
+public class StudentResponse {
+
+    private Long id;
+    private String name;
+    private Integer age;
+    private String gender;
+}
