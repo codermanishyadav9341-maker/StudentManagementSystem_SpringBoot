b3d48b421d60739017b838eaca7199056b22660d
Manish_Yadav_08 <codermanishyadav9341@gmail.com>
2026-07-21 17:52:41 +0530

    Initial commit

--- src/main/java/com/manish/studentmanagementsystem_springboot/mapper/StudentMapper.java
+++ src/main/java/com/manish/studentmanagementsystem_springboot/mapper/StudentMapper.java
@@ -6,13 +6,19 @@ import com.manish.studentmanagementsystem_springboot.dto.request.StudentRequest;
 import com.manish.studentmanagementsystem_springboot.dto.response.StudentResponse;
 //Import Student entity (DataBase table);
 import com.manish.studentmanagementsystem_springboot.entity.Student;
+import org.springframework.stereotype.Component;
 
+@Component
 public class StudentMapper { // StudentMapper-> dto or entity ke beech data convert karne ke liya use hota hai
     
     // StudentRequest DTO ko student entity me convert karta hai
     //Is method ka use new student save karne se phle hota hai;
     
     public static Student toEntity(StudentRequest request){
+
+        if(request == null){
+            return null;
+        }
         Student student = new Student();
         
         //DTO se entity me data set karna
@@ -22,6 +28,12 @@ public class StudentMapper { // StudentMapper-> dto or entity ke beech data conv
         student.setPhone(request.getPhone());
         student.setCourse(request.getCourse());
         student.setAge(request.getAge());
+        student.setAddress(request.getAddress());
+        student.setGender(request.getGender());
+        student.setDateOfBirth(request.getDateOfBirth());
+        student.setAdmissionDate(request.getAdmissionDate());
+        student.setFee(request.getFee());
+        student.setStatus(request.getStatus());
         
         return student;
     }
@@ -30,6 +42,10 @@ public class StudentMapper { // StudentMapper-> dto or entity ke beech data conv
     //Is method ka use clint ko response bhejne ke liya use hota hai;
     
     public static StudentResponse toResponse(Student student){
+
+        if(student == null){
+            return null;
+        }
         StudentResponse response = new StudentResponse();
         
         //Entity se DTO me data set karna
@@ -40,6 +56,12 @@ public class StudentMapper { // StudentMapper-> dto or entity ke beech data conv
         response.setPhone(student.getPhone());
         response.setCourse(student.getCourse());
         response.setAge(student.getAge());
+        response.setGender(student.getGender());
+        response.setAddress(student.getAddress());
+        response.setDateOfBirth(student.getDateOfBirth());
+        response.setAdmissionDate(student.getAdmissionDate());
+        response.setFee(student.getFee());
+        response.setStatus(student.getStatus());
         
         return response;
 
